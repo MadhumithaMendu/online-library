75282a0a486c3bbe00a76b464b0e90b8132b2a1e
madhumitha <mendumadhu338@gmail.com>
2026-04-03 17:24:50 +0530

    Enhance Navbar UI with improved layout and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="p-3 bg-dark text-white">
-      <Link className="text-white me-3" to="/">Home</Link>
-      <Link className="text-white me-3" to="/books/all">Browse Books</Link>
-      <Link className="text-white" to="/add">Add Book</Link>
+    <nav className="navbar">
+      <h3>📚 Library</h3>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/books/all">Browse</Link>
+        <Link to="/add">Add Book</Link>
+      </div>
     </nav>
   );
 }
